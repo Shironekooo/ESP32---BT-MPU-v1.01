9ae81fc1693c0bff626126b986a1b831e16370f7
Shironekooo <119466135+Shironekooo@users.noreply.github.com>
2023-04-29 18:41:44 +0800

    flex sensor angle added

--- src/main.cpp
+++ src/main.cpp
@@ -19,36 +19,39 @@ MPU6050 mpu;
 // GLOBAL VARIABLES
 int incomingByte = 0;
 
-float SensorReadingYaw, SensorReadingPitch, SensorReadingRoll, SensorReadingFlexResistance;
-bool deviceConnected = false;
+float SensorReadingYaw, SensorReadingPitch, SensorReadingRoll;
+float Flexangle;
+int   flexresistance;
+bool  deviceConnected = false;
 
 // MPU control/status vars
-bool dmpReady = false;   // set true if DMP init was successful
-uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
-uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;      // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64];  // FIFO storage buffer
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
-Quaternion q;         // [w, x, y, z]         quaternion container
-VectorInt16 aa;       // [x, y, z]            accel sensor measurements
-VectorInt16 gy;       // [x, y, z]            gyro sensor measurements
-VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;  // [x, y, z]            gravity vector
-float euler[3];       // [psi, theta, phi]    Euler angle container
-float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+Quaternion q;        // [w, x, y, z]         quaternion container
+VectorInt16 aa;      // [x, y, z]            accel sensor measurements
+VectorInt16 gy;      // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;  // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld; // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity; // [x, y, z]            gravity vector
+float euler[3];      // [psi, theta, phi]    Euler angle container
+float ypr[3];        // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
 
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
 
-volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
+volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
   mpuInterrupt = true;
 }
 
@@ -80,19 +83,19 @@ void initMPU()
   // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
-  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
 #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
   Fastwire::setup(400, true);
 #endif
 
   Serial.begin(115200);
   while (!Serial)
-    ;  // wait for Leonardo enumeration, others continue immediately
+    ; // wait for Leonardo enumeration, others continue immediately
 
   // initialize device
   Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
-  //pinMode(INTERRUPT_PIN, INPUT);
+  // pinMode(INTERRUPT_PIN, INPUT);
 
   // verify connection
   Serial.println(F("Testing device connections..."));
@@ -101,7 +104,7 @@ void initMPU()
   // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
-  
+
   // supply your own gyro offsets here, scaled for min sensitivity
   mpu.setXGyroOffset(51);
   mpu.setYGyroOffset(8);
@@ -110,7 +113,8 @@ void initMPU()
   mpu.setYAccelOffset(-50);
   mpu.setZAccelOffset(1060);
   // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
+  if (devStatus == 0)
+  {
     // Calibration Time: generate offsets and calibrate our MPU6050
     mpu.CalibrateAccel(6);
     mpu.CalibrateGyro(6);
@@ -133,7 +137,9 @@ void initMPU()
 
     // get expected DMP packet size for later comparison
     packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
+  }
+  else
+  {
     // ERROR!
     // 1 = initial memory load failed
     // 2 = DMP configuration updates failed
@@ -175,57 +181,60 @@ void initBLE()
 
 void setup()
 {
+  flexresistance = analogRead(FLEX_SENSOR_PIN);
   initMPU();
   initBLE();
 }
 
 void GyroSensorReadings()
 {
-if (!dmpReady) return;
-    // read a packet from FIFO
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+  if (!dmpReady)
+    return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
+  {
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    SensorReadingYaw    = ypr[0] * 180 / M_PI;
-    SensorReadingPitch  = ypr[1] * 180 / M_PI;
-    SensorReadingRoll   = ypr[2] * 180 / M_PI;
-}
+    SensorReadingYaw = ypr[0] * 180 / M_PI;
+    SensorReadingRoll = ypr[1] * 180 / M_PI;
+    SensorReadingPitch = ypr[2] * 180 / M_PI;
+  }
 }
 
 void FlexSensorReadings()
 {
-  float FlexSensorCalibration = 1730;
-  SensorReadingFlexResistance = (analogRead(FLEX_SENSOR_PIN) - FlexSensorCalibration);
+  int flexval = analogRead(FLEX_SENSOR_PIN);
+  Flexangle = map(flexval,flexresistance,flexresistance-530,90,0);
 }
 
 void NotifyDevice()
 {
-    // yaw
-    static char Yaw[100];
-    dtostrf(SensorReadingYaw, 6, 2, Yaw);
+  // yaw
+  static char Yaw[100];
+  dtostrf(SensorReadingYaw, 6, 2, Yaw);
 
-    // pitch
-    static char Pitch[100];
-    dtostrf(SensorReadingPitch, 6, 2, Pitch);
+  // pitch
+  static char Pitch[100];
+  dtostrf(SensorReadingPitch, 6, 2, Pitch);
 
-    // roll
-    static char Roll[100];
-    dtostrf(SensorReadingRoll, 6, 2, Roll);
+  // roll
+  static char Roll[100];
+  dtostrf(SensorReadingRoll, 6, 2, Roll);
 
-    // flex sensor resistance
-    static char Flex[100];
-    dtostrf(SensorReadingFlexResistance, 6, 2, Flex);
+  // flex sensor resistance
+  static char Flex[100];
+  dtostrf(Flexangle, 6, 2, Flex);
 
-    // final result
-    String result = String(Pitch) + "," + String(Roll) + "," + String(Flex);
-    int strlen = result.length() + 1;
+  // final result
+  String result = String(Pitch) + "," + String(Flex);
+  int strlen = result.length() + 1;
 
-    char readings[strlen];
-    result.toCharArray(readings, strlen);
-    Serial.println(readings);
-    mpuAccelerometerCharacteristics.setValue(readings);
-    mpuAccelerometerCharacteristics.notify();
+  char readings[strlen];
+  result.toCharArray(readings, strlen);
+  Serial.println(readings);
+  mpuAccelerometerCharacteristics.setValue(readings);
+  mpuAccelerometerCharacteristics.notify();
 }
 
 void CalibrateDevice()
@@ -248,14 +257,7 @@ void loop()
     FlexSensorReadings();
     NotifyDevice();
 
-    if (Serial.available() > 0)
-    {
-      incomingByte = Serial.read();
-      Serial.println("Calibrating...");
-      CalibrateDevice();
-    }
-
-    delay(100);
+    delay(10);
 
     if (!deviceConnected)
     {
